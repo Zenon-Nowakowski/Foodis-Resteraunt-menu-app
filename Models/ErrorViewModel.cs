f468b86cf3fefbdd33e06d8247b715aedab00031
Zenon-Nowakowski <zenon.nowakowski8@gmail.com>
2024-04-21 12:07:50 -0500

    init project

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace Foodis_Resteraunt_menu_app.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
